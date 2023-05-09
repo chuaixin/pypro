76c68f284a91c26c90a9e60799eef3c7a913cdcd
chuaixin <chuaixin@sina.com>
2023-05-09 22:53:14 +0800

    git test

--- tt.py
+++ tt.py
@@ -1,20 +1,19 @@
-#!/usr/bin/python3
-# filename  hello.py
-
+ 
 import pymysql
  
 # 连接数据库，创建连接对象connection
 # 连接对象作用是：连接数据库、发送数据库信息、处理回滚操作（查询中断时，数据库回到最初状态）、创建新的光标对象
  
-conn = pymysql.connect(
-    host='192.168.95.170',
-    user='khan2_pro',
-    password='khan2_pro',
-    database='khan2_pro',
-    charset='utf8')
+conn = pymysql.connect(host='127.0.0.1',  # host属性
+                       port=3306,  # 端口号
+                       user='root',  # 用户名
+                       password='007928',  # 此处填登录数据库的密码
+                       db='world' , # 数据库名
+                       charset="utf8"
+                       )
  
 # 执行完毕返回的结果集默认以元组显示 
-sql = "select * from kh_project limit 10"
+sql = "select * from country limit 10"
 
 # 执行SQL语句
 # 查询语句
