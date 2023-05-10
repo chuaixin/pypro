9ba9e53b67ca16124679aca44c33beb4e88460c9
chuaixin <chuaixin@sina.com>
2023-05-10 11:11:37 +0800

    test

--- flaskdo.py
+++ flaskdo.py
@@ -4,7 +4,8 @@ from flask import Flask,render_template
 app = Flask(__name__)
 @app.route('/')
 def index():
-   return render_template('index.html')
+   
+   return render_template('index.html',user = 'chuai')
 
 #启动这个WEB服务
 if __name__ == '__main__':
