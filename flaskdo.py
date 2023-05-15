a8de02a9a6e15d396c3c7fc27fea41031b263124
chuaixin <chuaixin@sina.com>
2023-05-15 21:41:27 +0800

    home commit

--- flaskdo.py
+++ flaskdo.py
@@ -1,10 +1,17 @@
 #从flask包中导入Flask类
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
-@app.route('/')
+@app.route('/',methods=['POST','GET'])
 def index():
-   return render_template('index.html')
+   pwd = request.values.get('pwd')
+   my_name = '测试一下cehnggong'+pwd
+   return render_template('index.html', myname=my_name)
+
+@app.route('/bootstrap-examples/album/')
+def boot():
+   return render_template('/bootstrap-examples/index.html')
+   print("test ok")
 
 #启动这个WEB服务
 if __name__ == '__main__':
