08883f00def4b62ad51cf6d79c4cfebaa130e00c
chuaixin <chuaixin@sina.com>
2023-10-26 15:23:38 +0800

    20231026 15:23

--- khan_dev_data.py
+++ khan_dev_data.py
@@ -218,6 +218,7 @@ def get_project_info(projectID):
 #项目团队成员及角色信息
 def get_project_memeber(projectID):
     conn = pymysql.connect(**conn_khan)
+    project_info = {}
     member_list = {}
     sql_members = "select ACCOUNT_,NAME_,DIC.`name` from kh_project_member AS PROJ JOIN kh_dict AS DIC ON PROJ.project_id=%s AND PROJ.`STATUS_`=1 AND PROJ.ROLE_ID = DIC.id" % projectID
     try:
@@ -228,15 +229,17 @@ def get_project_memeber(projectID):
         result = cursor.fetchall()
         for member in result:
             if not (member[0] in ignore_member):
-                member_list[member[0]]=member[2]
-        
+                member_list[member[0]]=member[2] 
     except Exception as e:
         print(e)
     # 关闭光标对象
     cursor.close()
     # 关闭数据库连接
     conn.close()
-    return member_list
+    project_info['membercount'] = len(member_list)
+    project_info['memberlist'] = member_list
+
+    return project_info
 
 # 获取项目版本及版本产品依赖信息
 def get_project_related(projectID):
@@ -251,41 +254,46 @@ def get_project_related(projectID):
         result = cursor.fetchall()
         vers_list = {}
         for version_info in result:
+            # 读取版本下的依赖产品配置
             sql_version = "select pv.project_version_id,khpro.name,khpro.project_key,pv.depend_project_id,pv.official_version,pv.upgrade_pack,pv.patch_pack FROM kh_project_version_dependency AS pv join kh_project AS khpro ON pv.project_version_id={0} AND khpro.id=pv.depend_project_id".format(version_info[1])
-            print(sql_version)
             cursor.execute(sql_version)
             result_version = cursor.fetchall()
+            
             if len(result_version)==0:
                 vers_list[version_info[0]]='' #将为空的版本信息加入字典
             else:
-                vers_depend_list = []
+                vers_depend_key=[]
                 # 根据版本数据统计该版本下依赖的版本
                 for vers_depend in result_version:
-                    
+                    vers_depend_list = []
                     # 添加主版本
                     if vers_depend[4] is not None:
-                        print("official",vers_depend[4])
                         vers_depend_list.append(vers_depend[4])
+
                     # 添加升级包版本
                     if vers_depend[5] is not None:
                         vers_upgrade = vers_depend[5].split(";&")
                         vers_upgrade = list(filter(None, vers_upgrade))
-                        print("upgrade".vers_upgrade)
                         vers_depend_list.extend(vers_upgrade)
                     # 添加补丁包版本
                     if vers_depend[6] is not None:
                         vers_patch = vers_depend[6].split(";&")
                         vers_patch = list(filter(None, vers_patch))
                         vers_depend_list.extend(vers_patch)
-                print(vers_depend_list)
-                
-                vers_list[version_info[0]]=vers_depend_list
-                # vers_depend_list.clear()
+                    # 对查询到的版本ID查询具体版本名称
+                    
+                    for vers_key in vers_depend_list:
+                        sql_verskey = "SELECT `name` FROM `khan2_pro`.`kh_project_version` WHERE `project_id` = {0} AND id = {1}".format(vers_depend[3],vers_key)
+                        cursor.execute(sql_verskey)
+                        result_version_key = cursor.fetchone()
+                        vers_temp_key = (vers_depend[2],vers_depend[1],result_version_key)
+                        vers_depend_key.append(vers_temp_key)
 
+                vers_list[version_info[0]]=vers_depend_key
 
-                    # print(vers_depend)  
+        project_info['verscount'] = len(vers_list)
+        project_info['relatelist'] = vers_list
 
-        project_info['产品依赖'] = vers_list
 
     except Exception as e:
         print(e)
@@ -294,6 +302,38 @@ def get_project_related(projectID):
     # 关闭数据库连接
     conn.close()
     return project_info
+
+def get_project_repository(projectID):
+    conn = pymysql.connect(**conn_khan)
+    project_info = {}
+
+    try:
+        cursor = conn.cursor()
+        #读取该项目的代码库
+        sql_repository_id = "select repository_id from kh_project__repository where project_id=%s" % projectID
+        cursor.execute(sql_repository_id)
+        result = cursor.fetchall()
+        repo_list = []
+        if len(result)>0:
+            for repo_id in result:
+                #通过代码库id查询提交代码次数
+                sql_sc_repository = "SELECT * FROM `khan2_pro`.`kh_gitlab_repository` WHERE `id`=%s " % repo_id[0]
+                cursor.execute(sql_sc_repository)
+                result_repo = cursor.fetchone()
+                repo_info = [result_repo[0],result_repo[5],result_repo[3]]
+                repo_list.append(repo_info)
+
+        project_info['repository_count'] = len(repo_list)
+        project_info['repository_list'] = repo_list
+
+    except Exception as e:
+        print(e)
+    # 关闭光标对象
+    cursor.close()
+    # 关闭数据库连接
+    conn.close()
+    return project_info
+
 
 # print(json.dumps(get_project_info(1010),indent=4,ensure_ascii=False))
 
--- measure.py
+++ measure.py
@@ -14,10 +14,13 @@ from khan_dev_data import *
 dname = '数字化运营事业部'
 print("【项目明细】：\n")
 project_list = get_project_by_dept("产品项目", dname)
-print("产品项目数：", len(project_list))
+print("项目数：", len(project_list))
 # print("明细：", project_list)
 for project_info in project_list:
-	# print(json.dumps(get_project_memeber(project_info[0]),indent=4,ensure_ascii=False))
-	print(json.dumps(get_project_related(107),indent=4,ensure_ascii=False))
+	print(project_info)
+
+	print("项目成员：",json.dumps(get_project_memeber(project_info[0]),indent=4,ensure_ascii=False))
+	print("项目版本及依赖：",json.dumps(get_project_related(project_info[0]),indent=4,ensure_ascii=False))
+	print("项目代码库：",json.dumps(get_project_repository(project_info[0]),indent=4,ensure_ascii=False))
 	break
 print("结束\n")
