52534f96a211daef533373530a6d31f201b10f0b
chuaixin <chuaixin@sina.com>
2024-06-21 18:16:56 +0800

    测试

--- hello.py
+++ hello.py
@@ -1,45 +1,16 @@
-#类定义
-class people:
-    #定义基本属性
-    name = ''
-    age = 0
-    #定义私有属性,私有属性在类外部无法直接进行访问
-    __weight = 0
-    #定义构造方法
-    def __init__(self,n,a,w):
-        self.name = n
-        self.age = a
-        self.__weight = w
-    def speak(self):
-        print("%s 说: 我 %d 岁。" %(self.name,self.age))
-
-#单继承示例
-class student(people):
-    grade = ''
-    def __init__(self,n,a,w,g):
-        #调用父类的构函
-        people.__init__(self,n,a,w)
-        self.grade = g
-    #覆写父类的方法
-    def speak(self):
-        print("%s 说: 我 %d 岁了，我在读 %d 年级"%(self.name,self.age,self.grade))
-
-#另一个类，多重继承之前的准备
-class speaker():
-    topic = ''
-    name = ''
-    def __init__(self,n,t):
-        self.name = n
-        self.topic = t
-    def speak(self):
-        print("我叫 %s，我是一个演说家，我演讲的主题是 %s"%(self.name,self.topic))
-
-#多重继承
-class sample(student):
-    a =''
-    def __init__(self,n,a,w,g,t):
-        student.__init__(self,n,a,w,g)
-        speaker.__init__(self,n,t)
-
-test = sample("Tim",25,80,4,"Python")
-test.speak()   #方法名同，默认调用的是在括号中排前地父类的方法
+import pandas as pd
+# 创建一个示例Series
+data = pd.Series(['United States', 'Canada', 'Mexico', 'Brazil', 'Argentina'])
+# 定义一个映射关系字典
+continent_mapping = {
+    'United States': 'North America',
+    'Canada': 'North America',
+    'Mexico': 'North America',
+    'Argentina': 'South America'
+}
+fill_defalt = 'Unknown' 
+print(continent_mapping)
+# 使用map方法进行映射
+data_mapped = data.map(continent_mapping).fillna(fill_defalt)   # 更正：添加缺失值填充  
+# 显示映射后的结果
+print(data_mapped)
